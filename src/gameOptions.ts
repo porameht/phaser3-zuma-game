0a7fe9edfbc8031e0f5c1cd6d2ad9779743a9821
fr4nk <porameht@outlook.com>
2024-08-25 17:28:36 +0700

    config game and add asset

---
+++ src/gameOptions.ts
@@ -0,0 +1,14 @@
+
+
+export const gameOptions: any = {
+  gameSize: {
+    width: 1920,
+    height: 1080,
+  },
+
+  gameBackgroundColor: 0x222222,
+
+  path: '{"type":"Path","x":0,"y":0,"autoClose":false,"curves":[{"type":"LineCurve","points":[1460,-50,1460,540]},{"type":"EllipseCurve","x":960,"y":540,"xRadius":500,"yRadius":500,"startAngle":0,"endAngle":180,"clockwise":false,"rotation":0},{"type":"EllipseCurve","x":910,"y":540,"xRadius":450,"yRadius":450,"startAngle":180,"endAngle":0,"clockwise":false,"rotation":0},{"type":"EllipseCurve","x":960,"y":540,"xRadius":400,"yRadius":400,"startAngle":0,"endAngle":180,"clockwise":false,"rotation":0},{"type":"EllipseCurve","x":910,"y":540,"xRadius":350,"yRadius":350,"startAngle":180,"endAngle":0,"clockwise":false,"rotation":0},{"type":"EllipseCurve","x":960,"y":540,"xRadius":300,"yRadius":300,"startAngle":0,"endAngle":180,"clockwise":false,"rotation":0},{"type":"EllipseCurve","x":910,"y":540,"xRadius":250,"yRadius":250,"startAngle":180,"endAngle":0,"clockwise":false,"rotation":0},{"type":"EllipseCurve","x":960,"y":540,"xRadius":200,"yRadius":200,"startAngle":0,"endAngle":180,"clockwise":false,"rotation":0},{"type":"EllipseCurve","x":910,"y":540,"xRadius":150,"yRadius":150,"startAngle":180,"endAngle":0,"clockwise":false,"rotation":0},{"type":"EllipseCurve","x":960,"y":540,"xRadius":100,"yRadius":100,"startAngle":0,"endAngle":180,"clockwise":false,"rotation":0}]}',
+  gemSpeed: 400,
+  gemRadius: 24
+};
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import Phaser from 'phaser';
+import { PreloadAssets } from './scenes/PreloadAssets';
+import { PlayGame } from './scenes/PlayGame';
+import { gameOptions } from './gameOptions';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const scaleObject : Phaser.Types.Core.ScaleConfig = {
+  mode: Phaser.Scale.FIT,
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+  parent: 'thegame',
+  width: gameOptions.gameSize.width,
+  height: gameOptions.gameSize.height
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const configObject: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  backgroundColor: gameOptions.gameBackgroundColor,
+  scale: scaleObject,
+  scene: [
+    PreloadAssets,
+    PlayGame
+  ]
+}
+
+new Phaser.Game(configObject);
